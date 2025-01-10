b0cb2a97e25d7aefda97d2a14ce026cd8504960f
Lamont Banks <lamontbanks@gmail.com>
2025-01-10 15:17:12 -0500

    Project completed
    Still working on a breadth first search

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ from maze import *
 def main():
     window = Window(800, 600)
 
-    maze = Maze(50, 50, 8, 10, 50, 50, window)
-    maze._create_cells()
-
-    maze._break_entrance_and_exit()
-    maze._break_walls_recursive(0, 0)
+    maze = Maze(50, 50, 14, 10, 50, 50, window)
+    maze.solve()
 
     window.wait_for_close()
 
